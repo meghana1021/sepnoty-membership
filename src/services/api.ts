7e704954b27f1b16747859c5c930dc95c9fb8c78
meghana1021 <meghana09102004@gmail.com>
2025-06-20 12:10:20 +0530

    done

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { Form, FormResponse } from '../types/form';
 
-const API_BASE_URL = 'http://localhost:3001/api';
+const API_BASE_URL = 'https://sepnoty-membership-1.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
